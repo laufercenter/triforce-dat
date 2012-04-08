5f954ff639ada2b6b670e7f7b205f7d8b1439ea8
nils <nils@376bb805-d709-4451-8b19-f41d6d19b301>
2012-04-08 17:21:37 +0000

    area precomputation script now does its job

--- surfaceIntegral.R
+++ surfaceIntegral.R
@@ -13,11 +13,11 @@
 
 
 
-res_lambda = 10
-res_psi = 10
-res_PHI = 10
+res_lambda = 35
+res_psi = 35
+res_PHI = 35
 
-max_lambda = pi
+max_lambda = pi/2
 max_psi = pi
 max_PHI = pi
 
@@ -53,9 +53,16 @@ normalAngle <-function(a,b){
 
 PHI2phi <- function(PHI, psi, lambda){
 
+	if(PHI==0){
+		res=0
+	}
+	else{
+	
+
 	#print("######################")
 
 	border = mvMultiply(rotz(-lambda), n)
+	v = mvMultiply(rotz(-psi), n)
 	#print(border)
 	#print("------------------")
 	a = border-n
@@ -65,8 +72,11 @@ PHI2phi <- function(PHI, psi, lambda){
 	#print(aRot)
 	#print("------------------")
 
+	ip = n + aRot
 
-	nIntersection = crossproduct(aRot,n)
+
+
+	nIntersection = crossproduct(v,ip)
 	#print(nIntersection)
 	#print("------------------")
 	nIntersection = normalize(nIntersection)
@@ -77,7 +87,10 @@ PHI2phi <- function(PHI, psi, lambda){
 	#print("------------------")
 	
 	#print("######################")
-	phiIntersection
+	res = phiIntersection
+	}
+	res
+
 }
 
 
@@ -89,79 +102,106 @@ n = matrix(c(1,0,0),3,1)
 nOrigin = matrix(c(0,0,1),3,1)
 
 
+csc <-function(x){
+	1/sin(x)
+}
+
+
 arcConvex <- function(phi, psi, lambda){
-	1+(-cos(lambda) * cos(psi)+sqrt(cos(phi)^2 * sin(psi)^2 * (-cos(lambda)^2+cos(psi)^2+cos(phi)^2 * sin(psi)^2)))/(cos(psi)^2+cos(phi)^2 * sin(psi)^2)
+	v = 1+(-cos(lambda) * cos(psi)+sqrt(cos(phi)^2 * sin(psi)^2 * (-cos(lambda)^2+cos(psi)^2+cos(phi)^2 * sin(psi)^2)))/(cos(psi)^2+cos(phi)^2 * sin(psi)^2)
+	#for(i in 1:length(v))
+	#	if(is.nan(v[i])) v[i]=0
+	v
 }
 
 arcConcave <- function(phi, psi, lambda){
-	1-(cos(lambda) * cos(psi)+sqrt(cos(phi)^2 * sin(psi)^2 * (-cos(lambda)^2+cos(psi)^2+cos(phi)^2 * sin(psi)^2)))/(cos(psi)^2+cos(phi)^2 * sin(psi)^2)	
+	v = 1-(cos(lambda) * cos(psi)+sqrt(cos(phi)^2 * sin(psi)^2 * (-cos(lambda)^2+cos(psi)^2+cos(phi)^2 * sin(psi)^2)))/(cos(psi)^2+cos(phi)^2 * sin(psi)^2)	
+	#for(i in 1:length(v))
+	#	if(is.nan(v[i])) v[i]=0
+	v
 }
 
+phiLimitNegative <-function(psi,lambda){
+	-acos(sqrt((cos(lambda)^2-cos(psi)^2) * csc(psi)^2))
+}
 
-dataConvex = array(0,dim=c(res_PHI+1,res_PHI+1,res_psi+1,res_lambda))
-dataConcave = array(0,dim=c(res_PHI+1,res_PHI+1,res_psi+1,res_lambda))
+phiLimitPositive <-function(psi,lambda){
+	acos(sqrt((cos(lambda)^2-cos(psi)^2) * csc(psi)^2))
+}
+
+findLimit <-function(psi,lambda){
+	r = 0
+	l = -pi
+
+	for(i in 0:1000){
+		m <- (l-r)/2
+		t <- arcConvex(m,psi,lambda)
+		if(is.nan(t)){
+			l<-m
+		}
+		else{
+			r<-m
+		}
+	}
+	r
+}
+
+dataConvex = array(0,dim=c(res_PHI+1,res_psi+1,res_lambda))
+dataConcave = array(0,dim=c(res_PHI+1,res_psi+1,res_lambda))
+
+
+pconvex <- function(psi,lambda){
+res=1000
+at = array(0,dim=c(res))
+h = array(0,dim=c(res))
+for(i in 1:res){
+	at[i]=arcConvex(max_PHI*2 * (i-(res/2))/res,psi,lambda)
+	h[i]=max_PHI*2 * (i-(res/2))/res
+}
+plot(h,at)
+}
 
 for(i_psi in 0:res_psi){
+	print(i_psi)
 	psi = max_psi*i_psi/res_psi
 	for(i_lambda in 1:(res_lambda-1)){
 		lambda = max_lambda*i_lambda/res_lambda
 
-		phiLimit0 = PHI2phi(-pi/2,psi,lambda)
-		phiLimit1 = PHI2phi(pi/2,psi,lambda)
+		phiLimit = PHI2phi(pi/2,psi,lambda)
 
-		for(i_PHI0 in 0:res_PHI){
-			PHI0 = max_PHI * (i_PHI0-(res_PHI/2))/res_PHI
+		for(i_PHI in 0:res_PHI){
+			PHI = max_PHI * i_PHI/res_PHI
 
-			phi0 = PHI2phi(PHI0,psi,lambda)
-
+			phi = PHI2phi(PHI,psi,lambda)
 
 
-			for(i_PHI1 in 0:res_PHI){
-				PHI1 = max_PHI * (i_PHI1-(res_PHI/2))/res_PHI
-
-				phi1 = PHI2phi(PHI1,psi,lambda)
-
-
-
-				#for the concave case
-				A=0
-				if(PHI0 < pi/4){
-					A = A + abs(integrate(arcConvex,lower=phi0,upper=phiLimit0,psi=psi,lambda=lambda)$val)
-					ip0=phiLimit0
-				}
-				else ip0=phi0
-
-				if(PHI1 > pi/4){
-					A = A + abs(integrate(arcConvex,lower=phi1,upper=phiLimit1,psi=psi,lambda=lambda)$val)
-					ip1=phiLimit1
-				}
-				else ip1=phi1
-				
-				A = A + abs(integrate(arcConcave,lower=ip0,upper=ip1,psi=psi,lambda=lambda)$val)
-
-				dataConcave[i_PHI0+1, i_PHI1+1, i_psi+1, i_lambda+1] = A
-
-				#for the convex case
-				A=0
-				if(PHI0 > pi/4){
-					A = A + abs(integrate(arcConcave,lower=phi0,upper=phiLimit0,psi=psi,lambda=lambda)$val)
-					ip0=phiLimit0
-				}
-				else ip0=phi0
-
-				if(PHI1 < pi/4){
-					A = A + abs(integrate(arcConcave,lower=phi1,upper=phiLimit1,psi=psi,lambda=lambda)$val)
-					ip1=phiLimit1
-				}
-				else ip1=phi1
-				
-				A = A + abs(integrate(arcConvex,lower=ip0,upper=ip1,psi=psi,lambda=lambda)$val)
-
-				dataConvex[i_PHI0+1, i_PHI1+1, i_psi+1, i_lambda+1] = A
-				
-
-				
+			#for the concave case
+			A=0
+			if(PHI > pi/2){
+				A = A + abs(integrate(arcConvex,lower=phi,upper=phiLimit,psi=psi,lambda=lambda)$val)
+				ip=phiLimit
 			}
+			else ip=phi
+			
+			A = A + abs(integrate(arcConcave,lower=0,upper=ip,psi=psi,lambda=lambda)$val)
+
+			dataConcave[i_PHI+1, i_psi+1, i_lambda+1] = A
+
+			#for the convex case
+			A=0
+			if(PHI < pi/2){
+				A = A + abs(integrate(arcConcave,lower=phi,upper=phiLimit,psi=psi,lambda=lambda)$val)
+				ip=phiLimit
+			}
+			else ip=phi
+			
+			A = A + abs(integrate(arcConvex,lower=0,upper=ip,psi=psi,lambda=lambda)$val)
+
+			dataConvex[i_PHI+1, i_psi+1, i_lambda+1] = A
+				
+
+				
+			
 		}
 	}
 }
